f57b8ac9b06898516965bbb829f2eabd443af580
Uzochukwu Ikemdinachi <uzochukwuikemdinachi@gmail.com>
2026-02-03 15:27:59 +0100

    changes

--- backend/app/emailService.py
+++ backend/app/emailService.py
@@ -1,11 +1,21 @@
 import smtplib
 import os
 from email.message import EmailMessage
+from dotenv import load_dotenv
+
+load_dotenv()
 
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
 EMAIL_USER = os.getenv("EMAIL_USER")
 EMAIL_PASS = os.getenv("EMAIL_PASS")
+
+# print("EMAIL CONFIG CHECK")
+# print("HOST:", EMAIL_HOST)
+# print("PORT:", EMAIL_PORT)
+# print("USER:", EMAIL_USER)
+# print("PASS SET:", EMAIL_PASS is not None)
+
 
 def send_email(to: str, subject: str, body: str):
     msg = EmailMessage()
@@ -13,7 +23,7 @@ def send_email(to: str, subject: str, body: str):
     msg["To"] = to
     msg["Subject"] = subject
     msg.set_content(body)
-    
+         
     with smtplib.SMTP_SSL(EMAIL_HOST, EMAIL_PORT, timeout=10) as server:
         server.login(EMAIL_USER, EMAIL_PASS)
         server.send_message(msg)
--- backend/app/scheduler.py
+++ backend/app/scheduler.py
@@ -1,71 +1,3 @@
-# from apscheduler.schedulers.background import BackgroundScheduler
-# from datetime import date
-# from sqlalchemy.orm import Session
-# from .database import SessionLocal
-# from .models import VehicleDocument, User
-# from .emailService import send_email
-
-# scheduler = BackgroundScheduler()
-
-# def check_document_expiries():
-#     print("Scheduler running at", date.today())
-
-#     """
-#     Runs daily.
-#     Checks all vehicle documents.
-#     Sends reminder emails to ALL registered users.
-#     """
-#     db: Session = SessionLocal()
-#     today = date.today()
-
-#     documents = db.query(VehicleDocument).all()
-#     users = db.query(User).all()  # get all registered users
-
-#     for doc in documents:
-#         days_left = (doc.expiry_date - today).days
-
-#         if days_left < 0:
-#             continue
-
-#         if days_left <= doc.reminder_start_days:
-#             users = db.query(User).all()  # get all registered users
-#             subject = f"{doc.document_type} Expiry Reminder"
-#             body = f"""
-# Hello,
-
-# This is a reminder that a vehicle document is nearing expiry.
-
-# Document: {doc.document_type}
-# Expiry Date: {doc.expiry_date}
-# Days Remaining: {days_left}
-
-# Please ensure renewal before the expiry date.
-# """
-
-#             # Send email to every registered user
-#             for user in users:
-#                 send_email(
-#                     to=user.email,
-#                     subject=subject,
-#                     body=body
-#                 )
-
-#     db.close()
-
-# def start_scheduler():
-#     if not scheduler.running:
-#         scheduler.add_job(
-#             check_document_expiries,
-#             trigger="interval",
-#             # days=1,
-#             seconds=10,
-#             id="document_expiry_job",
-#             replace_existing=True
-#         )
-#         scheduler.start()
-#         print("Scheduler started")
-
-
 from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import date
 from sqlalchemy.orm import Session
@@ -94,9 +26,7 @@ def check_document_expiries():
             if days_left <= doc.reminder_start_days:
                 if doc.last_notified_at == today:
                     continue  # Skip sending email if already notified today
-                doc.last_notified_at = today
-                db.commit()
-
+                
                 subject = f"{doc.document_type} Expiry Reminder"
 
                 for user in users:
@@ -117,6 +47,9 @@ Please ensure renewal before the expiry date.
                         subject=subject,
                         body=body
                     )
+                doc.last_notified_at = today
+                db.commit()
+
 
     except Exception as e:
         print("Scheduler error:", e)
@@ -129,7 +62,8 @@ def start_scheduler():
         scheduler.add_job(
             check_document_expiries,
             trigger="interval",
-            days=1,  # seconds for testing, days for prod
+            days=1,  # days for prod
+            # seconds=10,  # seconds for testing
             id="document_expiry_job",
             replace_existing=True,
             max_instances=1
