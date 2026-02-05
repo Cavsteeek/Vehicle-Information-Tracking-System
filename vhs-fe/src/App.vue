9411541615d86874195d8f990a10a57e9889a87d
Uzochukwu Ikemdinachi <uzochukwuikemdinachi@gmail.com>
2026-02-05 11:00:06 +0100

    changes

--- vhs-fe/src/App.vue
+++ vhs-fe/src/App.vue
@@ -1,5 +1,3 @@
 <template>
-  <div class="min-h-screen bg-black text-white flex items-center justify-center">
-    <h1 class="text-4xl font-bold">Tailwind OK</h1>
-  </div>
+  <router-view />
 </template>
